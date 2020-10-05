410af31e438e797fd3fef0095f66c1f537a601da
mderosa777 <meggsnbacon777@gmail.com>
2020-10-05 15:17:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const cards= ["Ada", "Brendan", "Ali"];
+
+function writeCards(cards,event){
+  let tyCards = []
+for(let i=0;i<cards.length;i++){
+tyCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+}
+return tyCards;
+}
+writeCards(cards);
+
+
+function countDown(num){
+  let i = num;
+while (i >= 0) {
+  console.log(i--);
+}
+ 
+return num;
+}
+
