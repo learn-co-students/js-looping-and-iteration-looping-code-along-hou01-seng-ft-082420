cb680cf4f962c7712b6af41cceb72479db4cd5bd
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-10-05 23:03:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(strings, event) {
+    let result = [];
+    for(let i = 0; i < strings.length; i++) {
+        let happyMessage = `Thank you, ${strings[i]}, for the wonderful ${event} gift!`;
+        result.push(happyMessage)
+    }
+    return result;
+}
+
+function countDown(number) {
+    let i = number;
+
+    while(i >= 0) {
+        console.log(i--);
+    }
+}
